f3d2a59d2a1d2ee57385fd6019500124f9ed2630
KassiaJRauen <kassia.grj@gmail.com>
2022-05-03 02:23:27 -0300

    correction

--- src/hooks/useAlert.ts
+++ src/hooks/useAlert.ts
@@ -1,11 +1,11 @@
-import {useContext} from "react";
-import {AlertContext} from "../contexts/AlertContext";
+import { useContext } from "react";
+import { AlertContext } from "../contexts/AlertContext";
 
-export default function userAlert(){
-    const alertContext = useContext(AlertContext);
-    if (!alertContext){
-        throw new Error("useAlert must be used inside a AlertContext Provider");
-    }
+export default function useAlert() {
+  const alertContext = useContext(AlertContext);
+  if (!alertContext) {
+    throw new Error("useAlert must be used inside a AlertContext Provider");
+  }
 
-    return alertContext;
+  return alertContext;
 }
